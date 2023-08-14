5739fc069bf4e3bd5e924531372aa03b238d0449
Joseph M <106897726+Jiffy-JM@users.noreply.github.com>
2023-08-14 18:55:56 -0400

    Create serverstart.sh

---
+++ serverstart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This will initiate the server and start loading.
+java -Xms1024M -Xmx1024M -jar minecraft_server_1.20.1.jar nogui
+
+# Reminder on how to detach from Screen Session
+echo "Remember to keep your screen instance active by detaching (Ctrl + A + D)."
