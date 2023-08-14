12d7518b146851efaac6aa98b10cee65875a2637
Joseph M <106897726+Jiffy-JM@users.noreply.github.com>
2023-08-14 18:54:04 -0400

    Update serverinstall.sh

--- serverinstall.sh
+++ serverinstall.sh
@@ -7,6 +7,7 @@ java -Xms1024M -Xmx1024M -jar minecraft_server_1.20.1.jar nogui
 sed -i 's/eula=false/eula=true/g' eula.txt
 
 # Reminder for user
+echo "The first time will fail.  Be sure to run it again, it needs to accept the EULA first."
 echo "Remember to keep your screen instance active by detaching (Ctrl + A + D)."
 echo "You can edit the server properties by editing the server.properties file with nano."
 echo "When you are ready to start your server, simply re-run:"
